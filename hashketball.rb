6239878a665e91de90b3c69d010b3881ca707692
Freda Hon <freda.hon@gmail.com>
2020-09-25 00:08:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -179,7 +179,7 @@ def player_numbers(team_name)
   game_hash.each do |team, team_info|
     if team_info[:team_name] == team_name
       team_info[:players].each do |player_info|
-        jeresey_array << player_info[:number]
+        jersey_array << player_info[:number]
       end
     end
   end
