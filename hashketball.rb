098f17149f0156cc787b91249cddf89c8664a8db
Freda Hon <freda.hon@gmail.com>
2020-09-25 00:16:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,7 +205,7 @@ def big_shoe_rebounds
   game_hash.each do |team, team_info|
     biggest_shoe = 0
     team_info[:players].each do |player_info|
-      
+      if player_info[:]
     end
   end
 end
