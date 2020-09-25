a7ecd9ab331323e195834f2480fbe52563d42d0a
Freda Hon <freda.hon@gmail.com>
2020-09-25 00:00:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,3 +162,9 @@ def team_colors(team_name)
     end
   end
 end
+
+# Build a method, player_numbers, that takes in an argument of a team name and returns an Array of the jersey numbers for that team.
+def player_numbers(team_name)
+  jersey_array = []
+  game_hash.each do |team, team_info
+end
